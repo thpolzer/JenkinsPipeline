293b852f78375b46d54fad756f187df560d6b605
thpolzer <tpolzer69@gmail.com>
2021-02-10 18:42:34 +0100

    last name added

--- maven.test/src/main/java/com/polzer/maven/test/App.java
+++ maven.test/src/main/java/com/polzer/maven/test/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "My name is Thomas");
+        System.out.println( "My last name is Polzer");
+
     }
 }
