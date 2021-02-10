293f9beefb24978a11f6135e6416b7c7d63961d2
thpolzer <tpolzer69@gmail.com>
2021-02-10 17:12:19 +0100

    first commit

---
+++ maven.test/src/main/java/com/polzer/maven/test/App.java
@@ -0,0 +1,13 @@
+package com.polzer.maven.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "My name is Thomas");
+    }
+}
